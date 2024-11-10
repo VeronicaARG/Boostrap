5a21dd7e938e07f00a9b5ec8cb6368fe88ceaa45
VeronicaARG <benaventeluca4@gmail.com>
2024-11-10 14:37:43 -0300

    Estos son todos mis archivos realizados durante Boostrap

---
+++ demas de java scrip/script.js
@@ -0,0 +1,13 @@
+let edad = parseInt(prompt("ingrese su edad"))
+edad = parseInt(edad)
+let variable = edad + 1
+alert("En un año tendras " + variable)
+
+let nombre = `lucila`
+let mensaje = `hola mi nombre es ${nombre}`
+alert(mensaje)
+
+console.log(`hola
+chau`)
+console.log()
+
---
+++ java scrip + tarea/script.js
@@ -0,0 +1,5 @@
+let palabra = prompt ("Ingrese una palabra.") 
+let resultado = palabra[0]
+let ultimaLetra = palabra [palabra.length-1]
+alert ("La inicial de su palabra es " + resultado + " y la ultima es " + ultimaLetra + ".")
+console.log("La inicial de su palabra es " + resultado + " y la ultima es " + ultimaLetra + ".")
